a687c6de640a4bd0a2478292b17e52b6e17a17fd
Jeremy R. Groves <jgroves@niu.edu>
2024-11-13 12:15:08 -0600

    Improve Code

--- Build/Code/Class Script.R
+++ Build/Code/Class Script.R
@@ -8,8 +8,10 @@ rm(list = ls())
 
 library(tidycensus)
 library(tidyverse)
-
 library(gt)
+library(gtsummary)
+library(sf)
+
 
 #Loading Data####
   ufo <- read.csv(file = "./Data/scrubbed.csv", header = TRUE, as.is = TRUE, sep = ",")
@@ -58,23 +60,39 @@ library(gt)
            state = as.factor(state),
            ln_n = log(n))
   
-
   
   ggplot(ufo.map) +
-    geom_boxplot(aes(x = decade, y = ln_n), color = "navy") + 
-    theme_bw() + 
+    geom_boxplot(aes(x = decade, y = ln_n), color = "navy")  + 
+    theme_bw()  + 
     labs(title = "Box Plot of UFO sightings by Decade in the U.S.",
-         caption = "Data obtained from Kaggle.com") +
-    xlab("Decade") +
-    ylab("Natural Log of Annual Sightings")
+         caption = "Data obtained from Kaggle.com",
+         x = "Decade",
+         y = "Natural Log of Annual Sightings")
   
-  ggplot(ufo.map) +
+  ggplot(subset(ufo.map, decade == 1990)) +
+    geom_sf(aes(fill = ln_n)) +
+    labs(title = "Natural Log of UFO Sightings by Decade in the U.S.",
+         caption = "Data obtained from Kaggle.com") 
+  
+  
+  ggplot(subset(ufo.map, decade == 1990)) +
     geom_sf(aes(fill = ln_n)) +
     labs(title = "Natural Log of UFO Sightings by Decade in the U.S.",
          caption = "Data obtained from Kaggle.com") +
-    scale_fill_gradient2(low = "white", high = "blue", na.value = NA, 
-                         name = "LN(Sightings)",
-                         limits = c(0, 9)) +
+    theme_bw()+
+    theme(axis.ticks = element_blank(),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          legend.position = "bottom",
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank()) 
+  
+  
+  ggplot(subset(ufo.map, decade == 1990)) +
+    geom_sf(aes(fill = ln_n)) +
+    labs(title = "Natural Log of UFO Sightings by Decade in the U.S.",
+         caption = "Data obtained from Kaggle.com") +
     theme_bw()+
     theme(axis.ticks = element_blank(),
           axis.text.x = element_blank(),
@@ -83,21 +101,66 @@ library(gt)
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           panel.border = element_blank()) +
+    scale_fill_gradient2(low = "lightblue", high = "blue", na.value = NA, 
+                         name = "LN(Sightings)",
+                         limits = c(0, 9)) 
+  ggplot(ufo.map) +
+    geom_sf(aes(fill = ln_n)) +
+    labs(title = "Natural Log of UFO Sightings by Decade in the U.S.",
+         caption = "Data obtained from Kaggle.com") +
+    theme_bw()+
+    theme(axis.ticks = element_blank(),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          legend.position = "bottom",
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank()) +
+    scale_fill_gradient2(low = "lightblue", high = "blue", na.value = NA, 
+                         name = "LN(Sightings)",
+                         limits = c(0, 9)) +
     facet_wrap( ~ decade)
               
-  
-
   
 
     
   c <- ufo.map %>% 
       st_drop_geometry() %>%
-      select(n, decade, state) %>%
-      rowwise() %>%
-      slice(rep(1, n)) %>%
-      select(-n) %>%
-      tbl_summary(
+      select(n, decade, State) %>%
+    as_tibble() %>%
+    rowwise() %>%
+    slice(rep(1, n)) %>%
+    select(-n) %>%
+    tbl_summary(
         by = decade,
-        statistic = list(all_continuous() ~ "{mean}"), missing = "no" ) %>%
-      modify_header(label ~ "**Variable**")
-      
+        statistic = list(all_categorical() ~ "{n}"), 
+        label = NULL,
+        missing = "no" )   %>%
+      modify_header(label ~ "**State**") %>%
+      remove_row_type(State, type = "header") %>%
+    as_gt() %>%
+    tab_header(title = md("**Table 1** Sightings by Decade for each State")) %>%
+    gtsave("Table 1.png", path = "./Analysis/Output/")
+  
+  d<-ufo.map %>%
+    st_drop_geometry() %>%
+    select(decade, n) %>%
+    tbl_summary(
+      by = decade,
+      type = all_continuous() ~ "continuous2",
+      statistic = all_continuous() ~ c("{mean}",
+                                       "{sum}",
+                                       "{N_obs}"),
+    ) %>%
+    add_stat_label(label = n ~ c("Mean", "Total", "States")) %>%
+    modify_header(all_stat_cols() ~ "**{level}**",
+                  label ~ "")  %>%
+    remove_row_type(n, type = "header") #%>%
+  as_gt() %>%
+    tab_header(title = md("**Table 2** Summary of Sightings")) %>%
+    gtsave("Table 2.png", path = "./Analysis/Output/")
+  
+
+  theme_gtsummary_compact()
+                       
+    
