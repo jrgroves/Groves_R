30e262ba607efaec265406028039c2da1e961a9f
Jeremy R. Groves <jgroves@niu.edu>
2024-11-20 15:18:39 -0600

    Final

--- Build/Code/Class Script.R
+++ Build/Code/Class Script.R
@@ -1,4 +1,4 @@
-#This script is used to input, clearn and prepare our UFO sighting data for use in analysis
+#This script is used to input, clean and prepare our UFO sighting data for use in analysis
 #
 #By: Jeremy Groves
 #Date: November 5, 2024
@@ -7,11 +7,9 @@ rm(list = ls())
 
 library(tidycensus)
 library(tidyverse)
-library(gt)
-library(gtsummary)
 library(sf)
-
-#census_api_key("9018e926f770ef85d125093f536ac4737154ce08", overwrite = FALSE, install = TRUE)
+library(stargazer)
+library(sf)
 
 #Loading Data####
   ufo <- read.csv(file = "./Data/scrubbed.csv", header = TRUE, as.is = TRUE, sep = ",")
@@ -45,10 +43,9 @@ library(sf)
     mutate(date = as.Date(str_split_i(datetime," ", 1), "%m/%d/%Y"),
            year = year(date)) %>%
     filter(year %in% years)
-  
-      
+
   cen.map <- cen.stat %>%
-    filter(year == "2020",
+    filter(year == "2011",
            GEOID != "02",
            GEOID != "15",
            GEOID != "72",
@@ -66,7 +63,7 @@ library(sf)
            smcol = (B12+B13+B29+B30) / B01,
            deg = (B14+B15+B31+B32) / B01,
            addeg = (B16+B17+B18+B33+B34+B35) / B01,
-           tpop = B01,
+           tpop = log(B01),
            year = as.numeric(year)) %>%
     select(year, GEOID, nohs:tpop)
    
@@ -81,29 +78,37 @@ library(sf)
     rename("GEOID" = "Code") %>%
     mutate(GEOID = str_pad(as.character(GEOID), width = 2, side = "left", pad="0"))
   
-  ufo.map <- cen.stats %>%
+  ufo.core <- cen.stats %>%
     left_join(., ufo.st, by=c("GEOID", "year")) %>%
     mutate(year = as.factor(year),
            state = as.factor(state),
-           ln_n = log(n))
+           ln_n = log(n)) %>%
+    filter(!is.na(state))
   
+  ufo.map <- cen.map %>%
+    select(-year) %>%
+    left_join(., ufo.core, by = "GEOID")
+  
+
+#Visualizations and Tables##### 
+
   ggplot(ufo.map) +
     geom_boxplot(aes(x = year, y = ln_n), color = "navy")  + 
     theme_bw()  + 
-    labs(title = "Box Plot of UFO sightings by Decade in the U.S.",
+    labs(title = "Box Plot of UFO sightings by Year in the U.S.",
          caption = "Data obtained from Kaggle.com",
-         x = "Decade",
+         x = "Year",
          y = "Natural Log of Annual Sightings")
   
   ggplot(subset(ufo.map, year == 2010)) +
     geom_sf(aes(fill = ln_n)) +
-    labs(title = "Natural Log of UFO Sightings by Decade in the U.S.",
+    labs(title = "Natural Log of UFO Sightings in the U.S.",
          caption = "Data obtained from Kaggle.com") 
   
   
   ggplot(subset(ufo.map, year == 2010)) +
     geom_sf(aes(fill = ln_n)) +
-    labs(title = "Natural Log of UFO Sightings by Decade in the U.S.",
+    labs(title = "Natural Log of UFO Sightings in the U.S.",
          caption = "Data obtained from Kaggle.com") +
     theme_bw()+
     theme(axis.ticks = element_blank(),
@@ -113,26 +118,10 @@ library(sf)
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           panel.border = element_blank()) 
-  
-  
-  ggplot(subset(ufo.map, decade == 1990)) +
-    geom_sf(aes(fill = ln_n)) +
-    labs(title = "Natural Log of UFO Sightings by Decade in the U.S.",
-         caption = "Data obtained from Kaggle.com") +
-    theme_bw()+
-    theme(axis.ticks = element_blank(),
-          axis.text.x = element_blank(),
-          axis.text.y = element_blank(),
-          legend.position = "bottom",
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank()) +
-    scale_fill_gradient2(low = "lightblue", high = "blue", na.value = NA, 
-                         name = "LN(Sightings)",
-                         limits = c(0, 9)) 
+ 
   ggplot(ufo.map) +
     geom_sf(aes(fill = ln_n)) +
-    labs(title = "Natural Log of UFO Sightings by Decade in the U.S.",
+    labs(title = "Natural Log of UFO Sightings by year in the U.S.",
          caption = "Data obtained from Kaggle.com") +
     theme_bw()+
     theme(axis.ticks = element_blank(),
@@ -145,87 +134,44 @@ library(sf)
     scale_fill_gradient2(low = "lightblue", high = "blue", na.value = "black", 
                          name = "LN(Sightings)",
                          limits = c(0, 9)) +
-    facet_wrap( ~ decade)
-              
-<<<<<<< HEAD
-  theme_gtsummary_compact()
+    facet_wrap( ~ year)
+ 
+  stargazer(as.data.frame(ufo.core), 
+            type = "latex", 
+            out = "./Analysis/Output/Table1.txt")
+  
+  
+  stargazer(as.data.frame(ufo.core), 
+            type = "html", 
+            out = "./Analysis/Output/Table1.html",
+            title="Table 1: Summary Statistics",
+            covariate.labels = c("No Highschool Degree", "Highschool Diploma","Some College",
+                                 "Undergraduate Degree","Advanced Degree","LN of Total Population",
+                                 "Sightings", "LN of Sightings"))
+  
+  
+ 
+#Regression Analysis####
+ 
+ mod1 <- lm(ln_n ~ nohs + hson + smcol + deg + tpop, robust,  data = ufo.core)
+ mod2 <- lm(ln_n ~ nohs + hson + smcol + deg + tpop + factor(year), data = ufo.core)
+ mod3 <- lm(ln_n ~ nohs + hson + smcol + deg + tpop + factor(year) + factor(State), data = ufo.core)
+  
+c<- stargazer::stargazer(mod1, mod2, mod3,omit = ".State.",
+                         type = "html",
+                         out = "./Analysis/Output/Table2.html",
+                         add.lines=list(c("State F.E.", "NO", "NO", "YES")),
+                         dep.var.labels = "LN(Sightings)",
+                         covariate.labels=c("No Highschool Degree", "Highschool Diploma","Some College",
+                                            "Undergraduate Degree","Total Population", "2011 F.E.", "2012 F.E."))
 
-=======
-  
-  theme_gtsummary_compact()
->>>>>>> 2012ea911e7a0e5dfb4c001865e33f1e39c00d8f
-    
-  c <- ufo.map %>% 
-      st_drop_geometry() %>%
-      select(n, decade, State) %>%
-    as_tibble() %>%
-    rowwise() %>%
-    slice(rep(1, n)) %>%
-    select(-n) %>%
-    tbl_summary(
-        by = decade,
-        statistic = list(all_categorical() ~ "{n}"), 
-        label = NULL,
-        missing = "no" )   %>%
-      modify_header(label ~ "**State**") %>%
-      remove_row_type(State, type = "header") %>%
-    as_gt() %>%
-    tab_header(title = md("**Table 1** Sightings by Decade for each State")) %>%
-    gtsave("Table 1.png", path = "./Analysis/Output/")
-  
-  d<-ufo.map %>%
-    st_drop_geometry() %>%
-    select(decade, n) %>%
-    tbl_summary(
-      by = decade,
-      type = all_continuous() ~ "continuous2",
-      statistic = all_continuous() ~ c("{mean}",
-                                       "{sum}",
-                                       "{N_obs}"),
-    ) %>%
-    add_stat_label(label = n ~ c("Mean", "Total", "States")) %>%
-    modify_header(all_stat_cols() ~ "**{level}**",
-                  label ~ "")  %>%
-    remove_row_type(n, type = "header") %>%
-  as_gt() %>%
-    tab_header(title = md("**Table 2** Summary of Sightings")) %>%
-    gtsave("Table 2.png", path = "./Analysis/Output/")
-  
+library(jtools)
+coef <- c("No Highschool Degree" = "nohs", "Highschool Diploma" = "hson","Some College" = "smcol",
+          "Undergraduate Degree" = "deg","LN of Total Population" = "tpop", 
+          "Fixed Effect: 2011" = "factor(year)2011",
+          "Fixed Effect: 2012" = "factor(year)2012")
 
-<<<<<<< HEAD
-#Regressions
-  library(ipumsr)
-  
-  data_ext<- define_extract_nhgis(
-    description = "ECON 691",
-    time_series_tables =  list(
-      tst_spec("A00", "state"),
-      tst_spec("B57", "state"),
-    )
-  )
-  
-  ts<-submit_extract(data_ext)
-  wait_for_extract(ts)
-  filepath <- download_extract(ts)
-  
-  dat <- read_nhgis(filepath)
-=======
+plot_summs(mod1, mod2, mod3, coefs = coef)
 
->>>>>>> 2012ea911e7a0e5dfb4c001865e33f1e39c00d8f
-                       
-  dat2 <- dat %>%
-    select(STATEFP, ends_with(c("1970", "1980", "1990", "2000", "2010", "105", "2020", "205"))) %>%
-    filter(!is.na(STATEFP)) %>%
-    pivot_longer(!STATEFP, names_to = "series", values_to = "estimate") %>%
-    mutate(series = str_replace(series, "105", "2010"),
-           series = str_replace(series, "205", "2020"),
-           year = substr(series, 6, nchar(series)),
-           series = substr(series, 1, 5)) %>%
-    distinct(STATEFP, series, year, .keep_all = TRUE) %>%
-    filter(!is.na(estimate)) %>%
-    pivot_wider(id_cols = c(STATEFP, year), names_from = series, values_from = estimate) %>%
-    mutate(und18 = rowSums(across(B57AA:B57AD)) / A00AA,
-           over65 = rowSums(across(B57AP:B57AR)) / A00AA) %>%
-    select(STATEFP, year, A00AA, und18:over65) %>%
-    rename("TOTPOP" = "A00AA",
-           "GEOID" = "STATEFP")
+export_summs(mod1, mod2, mod3, coefs = coef,
+             to.file = "docx", file.name = "./Analysis/Output/Reg.docx")
