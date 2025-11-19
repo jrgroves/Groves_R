01ccbd51e559c8efd892f94e5edccbf3126f6f51
Jeremy R. Groves <jgroves@niu.edu>
2025-11-19 15:15:29 -0600

    Core Data

---
+++ Build/Code/First Script.R
@@ -0,0 +1,6 @@
+#This is a script to introduce the idea of scripting
+#Jeremy R. Groves
+#November 5, 2025
+
+rm(list=ls())
+
--- Build/Code/Working.R
+++ Build/Code/Working.R
@@ -5,58 +5,60 @@
 
 rm(list = ls())
 
-library(tidycensus)
+library(tidyverse)
 library(sf)
 library(tidycensus)
 
 #Getting the Data####
 
   ufo <- read.csv("./Build/Input/scrubbed.csv", header = TRUE, as.is = TRUE)
-  
+  bridge <- read.csv("./Data/GEOID Bridge.csv", header = TRUE, as.is = TRUE)
   var <- c("B01002_001", "B01003_001")
-  states <- c(17, 18, 21)
-  
+
+for(i in seq(2009,2011,1)) {  
   acs <- get_acs(geography = "state",
                  variables = var,
-                 state = states,
-                 year = 2010,
-                 geometry = TRUE)
+                 year = i,
+                 geometry = TRUE) %>%
+    mutate(year = i)
   
+  
+  ifelse(i == 2009,
+         ACS <- acs,
+         ACS <- bind_rows(ACS, acs))
+}
+
 #Cleaning the Data
   
   working <- ufo %>%
     select(datetime, city, state, country, latitude, longitude) %>%
     filter(country == "us") %>%
     mutate(year = str_split_i(str_split_i(datetime, " ", 1), "/", 3)) %>%
-    filter(state == "in" | state == "il" | state == "ky",
-           year == 2010) 
-  
-  census <- acs %>%
-    select(GEOID, variable, estimate, geometry) %>%
+    mutate(year = as.numeric(year)) %>%
+    filter(year > 2008) |>
+    count(year, state) |>
+    rename("sightings" = "n")
+
+  census <- ACS %>%
+    select(GEOID, variable, year, estimate, geometry) %>%
     mutate(variable = case_when(variable == "B01002_001" ~ "median.age",
                                 variable == "B01003_001" ~ "population",
-                                TRUE ~ variable),
-           state = case_when(GEOID == 17 ~ "il",
-                             GEOID == 18 ~ "in",
-                             TRUE ~ "ky"))  %>%
-    pivot_wider(id_cols = c(GEOID, state, geometry), names_from = variable, values_from = estimate) %>%
+                                TRUE ~ variable))  %>%
+    pivot_wider(id_cols = c(GEOID, year, geometry), names_from = variable, values_from = estimate) %>%
     mutate(population = population / 100000)
   
+  bridge <- bridge %>%
+    mutate(GEOID = str_pad(as.character(GEOID),2, side = "left", pad = "0" ))
+  
   core <- census %>%
-    left_join(., sightings, by = "state") %>%
-    mutate(sight_per = n / population) %>%
+    left_join(., bridge, by = "GEOID") %>%
+    left_join(., working, by = c("state", "year")) %>%
+    mutate(sight_per = sightings / population) %>%
     relocate(geometry, .after = sight_per)
   
   
   
   
-  
-  
-  
-  
-  
-  sightings <- working %>%
-    count(state)
   
   
   
